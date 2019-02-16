6d9cce82f2524a2611eab9fc39fb5136e5eb1d1e
winhackger <winhackger@gmail.com>
2019-02-16 13:24:34 +0700

    pass data from bodyparser

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,20 @@
 let express = require("express")
 const app = express()
+let bodyParser = require("body-parser")
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/status', (req, res) => {
     res.send("Hello server nodejs")
+})
+
+app.get('/hello/:person', (req, res) => {
+    res.send("say hello with "+ req.params.person)
+})
+
+app.post('/hello', (req, res) => {
+    res.send("OH post - "+ req.body.name)
 })
 
 let port = 8080
